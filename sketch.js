26b41881043244bdd89cf0e5e815eeed6e4eb4c8
ShagunBuraniya <79150396+ShagunBuraniya@users.noreply.github.com>
2021-09-03 09:46:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ function setup() {
 	//create fairy sprite and add animation for fairy
 	fairy = createSprite(130,520);
 	fairy.addAnimation("flyingfairy",fairyImage);
-	fairy.scale = 0.4;
+	fairy.scale = 0.3;
 
 
 	star = createSprite(650,30);
